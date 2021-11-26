4e23862a125c3dc28041d1d7b2e28d7b8d417f24
nanak <nanaksethh@gmail.com>
2021-11-26 23:12:10 +0530

    canvas settings

--- next.config.js
+++ next.config.js
@@ -4,3 +4,14 @@ module.exports = {
       domains: ["localhost:8000", "localhost"],
    },
 };
+
+if (
+   process.env.LD_LIBRARY_PATH == null ||
+   !process.env.LD_LIBRARY_PATH.includes(
+     `${process.env.PWD}/node_modules/canvas/build/Release:`,
+   )
+ ) {
+   process.env.LD_LIBRARY_PATH = `${
+     process.env.PWD
+   }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+ }
