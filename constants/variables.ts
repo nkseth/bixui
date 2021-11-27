147650dcfb2d756d7441a367242e97ac601c2623
nanak <nanaksethh@gmail.com>
2021-11-27 15:33:29 +0530

    new changes

--- constants/variables.ts
+++ constants/variables.ts
@@ -1,5 +1,5 @@
-export const APP_URI = process.env.NODE_ENV === "production" ? "" : "http://localhost:5000";
+export const APP_URI = process.env.NODE_ENV === "production" ? "" : "http://localhost:3000";
 export const API_URI = "https://api.bizphotobooth.com";
-//export const API_URI = "http://localhost:5000";
+//export const API_URI = "http://localhost:8000";
 
 export const IMAGES_URI = API_URI + "/images/";
